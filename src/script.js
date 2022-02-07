f1b6fa35db78005d5f6e99c68df1dd644b17da54
alison-g <ali.grove87@gmail.com>
2022-02-07 18:27:07 +0000

    JS function for ℉ and ℃ button to to "feels like"

--- src/script.js
+++ src/script.js
@@ -10,6 +10,16 @@ function changeTempToF(event) {
   removeC.innerHTML = "℉";
 }
 
+function changeFeelsLikeTempToF(event) {
+  event.preventDefault();
+  let feelsLike = document.querySelector("#feelsLike");
+  let removeC = document.querySelector("#feelsLikeC");
+  let temp = feelsLike.innerHTML;
+
+  feelsLike.innerHTML = Math.round((temp * 9) / 5) + 32;
+  removeC.innerHTML = "℉";
+}
+
 function changeTempToC(event) {
   event.preventDefault();
   let tempTwo = document.querySelector("#mainTemp");
@@ -20,8 +30,24 @@ function changeTempToC(event) {
   removeF.innerHTML = "℃";
 }
 
+function changeFeelsLikeTempToC(event) {
+  event.preventDefault();
+  let feelsLike = document.querySelector("#feelsLike");
+  let removeF = document.querySelector("#feelsLikeC");
+  let temp = feelsLike.innerHTML;
+
+  feelsLike.innerHTML = Math.round(((temp -= 32) * 5) / 9);
+  removeF.innerHTML = "℃";
+}
+
 let switchToF = document.querySelector("#temp");
 switchToF.addEventListener("click", changeTempToF);
+
+let switchFeelsLikeToF = document.querySelector("#temp");
+switchFeelsLikeToF.addEventListener("click", changeFeelsLikeTempToF);
+
+let switchFeelsLikeToC = document.querySelector("#tempC");
+switchFeelsLikeToC.addEventListener("click", changeFeelsLikeTempToC);
 
 let switchToC = document.querySelector("#tempC");
 switchToC.addEventListener("click", changeTempToC);

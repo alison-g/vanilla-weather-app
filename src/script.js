1922b919d94a6e7e48ef6379677d5eb9fa1b1917
alison-g <ali.grove87@gmail.com>
2022-02-07 18:13:47 +0000

    JS function for ℉ button

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
+// C to F
 
+function changeTempToF(event) {
+  event.preventDefault();
+  let tempOne = document.querySelector("#mainTemp");
+  let removeC = document.querySelector("#mainC");
+  let temp = tempOne.innerHTML;
+
+  tempOne.innerHTML = Math.round((temp * 9) / 5) + 32;
+  removeC.innerHTML = "℉";
+}
+
+let switchToF = document.querySelector("#temp");
+switchToF.addEventListener("click", changeTempToF);
